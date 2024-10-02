1ba7e6a12282b4e620233b4a54d1b2b85341163f
Peanut <nutthunsk@gmail.com>
2024-10-02 21:11:49 +0700

    z

--- Example_data.py
+++ Example_data.py
@@ -6,35 +6,50 @@ import struct, os
 #Create
 def create_records(file):
 
-    with open(file) as file:
+    with open(file, "wb") as file:
         pass
 
 #Add
 def add_records(file):
 
-    with open(file) as file:
+    with open(file, "ab") as file:
         pass
 
 #Edit
 def edit_record(file):
+        check = os.path.exists(file)
+        if check != True:
+             print("Error: File Not Found!")
         pass
 
 #Read
 def read_records(file) ->str:
 
-    with open(file) as file:
+    with open(file, "rb") as file:
         pass
 
 #Find
 def find_records(file) ->str:
 
-    with open(file) as file:
+    with open(file, "rb") as file:
         pass
 
 #Remove
 def remove_record(file):
+        check = os.path.exists(file)
+        if check != True:
+             print("Error: File Not Found!")
         pass
 
 #Delete
 def delete_record(file):
-        pass
+        check = os.path.exists(file)
+        if check != True:
+                print("Error: File Not Found!!")
+        else:
+                q = input("Confirm type (Yes,No): ")
+                if q.lower() == "yes":
+                        os.remove(filePath)
+                        print(f"{filePath} Removed!!")
+                else:
+                        print("Removing has been Stoped!!")
